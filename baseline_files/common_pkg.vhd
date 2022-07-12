c13135230977c95c47f8263ac09b159d21ab93c7
Sawii00 <s.nasturzio@libero.it>
2022-07-12 17:50:14 +0200

    Removed unused old files

--- baseline_files/common_pkg.vhd
+++
@@ -1,8 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-package common_utils_pkg is
-    TYPE WORD_ARR IS ARRAY(79 DOWNTO 0) OF unsigned(31 DOWNTO 0);
-end package;
-    
